3136fd6d42c29cfd04ad594c8e930eb1bd45d2d2
Rafal Kluszczynski <klusi@icm.edu.pl>
2017-02-13 12:33:14 +0100

    test for normalize and save it to file

--- src/test/groovy/io/github/rkluszczynski/avro/cli/command/NormalizationCommandTest.groovy
+++ src/test/groovy/io/github/rkluszczynski/avro/cli/command/NormalizationCommandTest.groovy
@@ -7,12 +7,26 @@ import io.github.rkluszczynski.avro.cli.command.normalization.AvroSchemaNormaliz
 class NormalizationCommandTest extends BaseTestSpecification {
     private commandService = new CliCommandService([new AvroSchemaNormalization()])
 
-    def 'should normalize schema to canonical parsing form'() {
+    def 'should normalize schema to canonical parsing form and print to stdout'() {
         when:
         commandService.executeCommand('normalize', '--schema', prepareSchemaPath('schema-fat.avsc'))
 
         then:
         trimmedOutput() == '{"name":"io.github.rkluszczynski.avro.cli.testRecord","type":"record","fields":[]}'
+    }
+
+    def 'should normalize schema to canonical parsing form and save it to file'() {
+        given:
+        def tmpFile = File.createTempFile('schema', '.avsc')
+
+        when:
+        commandService.executeCommand('normalize',
+                '--schema', prepareSchemaPath('schema-fat.avsc'),
+                '--outputFile', tmpFile.canonicalPath
+        )
+
+        then:
+        compareFilesContent(prepareSchemaPath('schema-normalized.avsc'), tmpFile.canonicalPath)
     }
 
     private prepareSchemaPath(schemaFilename) {
